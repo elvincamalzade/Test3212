69d9ca84a8a24f98f29cdef6e1919956450664d2
e.camalzade <e.camalzade@STHQ0124-01>
2022-03-06 16:47:45 +0400

    Add project files.

---
+++ Project4/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+void mysetcolor(int fg, int bg) {
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(h, fg | (bg << 4));
+}
+void main() {
+	cout << "Salam" << endl;
+	mysetcolor(4, 0);
+	cout << "Salam" << endl;
+	mysetcolor(7, 0);
+	cout << "Salam" << endl;
+}
